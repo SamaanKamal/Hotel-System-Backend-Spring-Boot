e98c27ac6061e735319883dddcf0fc15d2ce1a7f
Samaan Kamal <samaankamal72@gmail.com>
2024-07-04 16:47:44 +0300

    intialized project with some dependacies

---
+++ src/main/java/com/example/Hotel/System/demo/HotelSystemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotel.System.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSystemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSystemDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hotel/System/demo/HotelSystemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotel.System.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSystemDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
