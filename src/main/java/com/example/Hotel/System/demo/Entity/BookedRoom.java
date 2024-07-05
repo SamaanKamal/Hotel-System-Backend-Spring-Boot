a9768853233b58047449cd665350f5feaad4220d
Samaan Kamal <samaankamal72@gmail.com>
2024-07-05 18:05:59 +0300

    added Booking Info for the Rooms

---
+++ src/main/java/com/example/Hotel/System/demo/Entity/BookedRoom.java
@@ -0,0 +1,45 @@
+package com.example.Hotel.System.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "booked_room")
+public class BookedRoom {
+    private Integer bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullName")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int NumOfAdults;
+
+    @Column(name = "children")
+    private int NumOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+}
