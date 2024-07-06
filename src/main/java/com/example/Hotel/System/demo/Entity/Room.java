787e6ba53fc1b79a90443eb9df24e96ab88aff67
Samaan Kamal <samaankamal72@gmail.com>
2024-07-06 21:32:41 +0300

    added unidirectional one to many relationship with list to bookings and method for adding new booking

--- src/main/java/com/example/Hotel/System/demo/Entity/Room.java
+++ src/main/java/com/example/Hotel/System/demo/Entity/Room.java
@@ -2,9 +2,12 @@ package com.example.Hotel.System.demo.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.math.BigDecimal;
 import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -32,4 +35,20 @@ public class Room {
     @Column(name = "photo")
     @Lob
     private Blob photo;
+    @OneToMany(mappedBy="room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+    public void addBooking(BookedRoom booking){
+        if (bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
