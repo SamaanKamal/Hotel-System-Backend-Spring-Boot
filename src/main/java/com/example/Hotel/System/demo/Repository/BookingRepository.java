dae2eec0fdc0a60f06c7c3ad4ac4f45576ab7c38
Samaan Kamal <samaankamal72@gmail.com>
2025-07-05 23:26:43 +0300

    added repository for booking and room

--- src/main/java/com/example/Hotel/System/demo/Repository/BookingRepository.java
+++ src/main/java/com/example/Hotel/System/demo/Repository/BookingRepository.java
@@ -2,6 +2,8 @@ package com.example.Hotel.System.demo.Repository;
 
 import com.example.Hotel.System.demo.Entity.BookedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookingRepository extends JpaRepository<BookedRoom,Integer> {
 }
--- src/main/java/com/example/Hotel/System/demo/Repository/RoomRepository.java
+++ src/main/java/com/example/Hotel/System/demo/Repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.example.Hotel.System.demo.Repository;
 
 import com.example.Hotel.System.demo.Entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room,Integer> {
 }
